b69b4d5fc56aa511ef977874793e60b0f4ce2fd5
Jhonatan Hulse <jhonatan.hulse@gmail.com>
2017-08-01 21:35:14 -0300

    Add migration for creating table of products

---
+++ database/migrations/2017_08_02_000507_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->decimal('price');
+            $table->string('name');
+            $table->text('description');
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
