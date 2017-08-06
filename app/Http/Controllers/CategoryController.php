42e2fbbf2bfaa4c4439401d8a9d47f1ef701b4db
Jhonatan Hulse <jhonatan.hulse@gmail.com>
2017-08-05 22:26:06 -0300

    Add category creation form

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -24,7 +24,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('categories.create');
     }
 
     /**
@@ -35,7 +35,18 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required|string|max:255|unique:categories,name'
+        ]);
+
+        Category::create($request->all());
+
+        return redirect()
+            ->route('categories.index')
+            ->with('alert', [
+                'type' => 'success',
+                'message' => 'Category created'
+            ]);
     }
 
     /**
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -2,6 +2,28 @@
 
 @section('content')
 <div class="container">
+    @if (session('alert'))
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="alert alert-{{ session('alert')['type'] }}">
+                    {{ session('alert')['message'] }}
+
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    @endif
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <nav class="navbar text-right">
+                <a href="{{ route('categories.create') }}" class="btn btn-success">Create</a>
+            </nav>
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
