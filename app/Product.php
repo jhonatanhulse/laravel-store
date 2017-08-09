d3cf5a49bb46ca3b0b04cc9188c993cbf0069947
Jhonatan Hulse <jhonatan.hulse@gmail.com>
2017-08-08 22:04:14 -0300

    Add product model

---
+++ app/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'price',
+        'description'
+    ];
+
+    /**
+     * Get the category of this product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
